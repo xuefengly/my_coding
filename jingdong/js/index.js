ab21dcedb3f2724767df9dbb2e91ebd0629da3c2
lyxuefeng <1316939854@qq.com>
2018-07-23 23:01:11 +0800

    studying

--- jingdong/js/index.js
+++
@@ -1,6 +0,0 @@
-var life_icons = document.getElementById('lifeservice-icon');
-var icons = life_icons.getElementsByTagName('i');
-// console.log(i);
-for(var i=0;i<icons.length;i++){
-	icons[i].style.backgroundPosition = "0 "+(-25*i)+"px";
-}
