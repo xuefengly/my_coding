afc3b080f13197483bafe05174d5d36a8813cb43
lyxuefeng <1316939854@qq.com>
2018-08-18 21:31:35 +0800

    ninth day ending

---
+++ jingdong/其他学习/网页特效或小技巧/小火箭返回顶部/my.js
@@ -0,0 +1,30 @@
+function $(id){
+	return document.getElementById(id);
+}
+function show(obj){
+	obj.style.display = "block";
+}
+function hide(obj){
+	obj.style.display = "none";
+}
+function scroll(){
+	if(window.pageYOffset != null){
+		//ie9+和各种最新浏览器，注意：为什么使用！=null,因为加载完页面pageYOffset就是0
+		return {
+			left: window.pageXOffset,
+			top: window.pageYOffset
+		}
+	}else if(document.compatMode == "CSS1Compat"){
+		//不是怪异模式(即声明了DTD)
+		return {
+			left: document.documentElement.scrollLeft,
+			top: document.documentElement.scrollTop
+		}
+	}else{
+		//怪异模式(未声明DTD)
+		return {
+			left: document.body.scrollLeft,
+			top: document.body.scrollTop
+		}
+	}
+}
